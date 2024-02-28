f764f6335b0e01112ba780a66926a9bdff00ef89
renatocouto1867 <renatocouto1867@gmail.com>
2024-02-28 01:21:35 -0300

    final

--- app/src/main/java/com/example/appimc/Calculos.java
+++ app/src/main/java/com/example/appimc/Calculos.java
@@ -4,28 +4,29 @@ import java.text.DecimalFormat;
 
 public class Calculos {
     private static double calcularIMC(Double altura, Double peso) {
-        DecimalFormat decimalFormat = new DecimalFormat("#.##");
-        return Double.parseDouble(decimalFormat.format( peso / (altura * altura)));
+
+        return (peso / (altura * altura));
     }
 
-    public static String classificaIMC(Double altura, Double peso){
-        double imc = calcularIMC(altura,peso);
+    public static String classificaIMC(Double altura, Double peso) {
+        double imc = calcularIMC(altura, peso);
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
-        if (imc <18.5){
-            return "Seu IMC é "+imc + ", Você esta abaixo do peso";
+        if (imc < 18.5) {
+            return "Seu IMC é " + decimalFormat.format(imc) + "\nVocê esta abaixo do peso";
         }
-        if (imc <25){
-            return "Seu IMC é "+imc + ", Você esta com o peso adequado";
+        if (imc < 25) {
+            return "Seu IMC é " + decimalFormat.format(imc) + "\nVocê esta com o peso adequado";
         }
-        if (imc <30){
-            return "Seu IMC é "+imc + ", Você esta sobrepeso";
+        if (imc < 30) {
+            return "Seu IMC é " + decimalFormat.format(imc) + "\nVocê esta sobrepeso";
         }
-        if (imc <35){
-            return "Seu IMC é "+imc + ", Você esta com obesidade grau 1";
+        if (imc < 35) {
+            return "Seu IMC é " + decimalFormat.format(imc) + "\nVocê esta com obesidade grau 1";
         }
-        if (imc <40){
-            return "Seu IMC é "+imc + ", Você esta com obesidade grau 2";
+        if (imc < 40) {
+            return "Seu IMC é " + decimalFormat.format(imc) + "\nVocê esta com obesidade grau 2";
         }
-        return "Seu IMC é "+imc + ", Você esta com obesidade grau 3";
+        return "Seu IMC é " + decimalFormat.format(imc) + "\nVocê esta com obesidade grau 3";
     }
 }
